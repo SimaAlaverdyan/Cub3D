00fea8d973840a94df95f0fa7173118baacfce8f
Zaruhi Hatsagortsyan <zhatsago@c2r5s2.42yerevan.am>
2022-08-27 21:08:30 +0400

    added new map validations checking first row, last column

--- src/check_borders.c
+++ src/check_borders.c
@@ -6,7 +6,7 @@
 /*   By: zhatsago <zhatsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 21:30:02 by zhatsago          #+#    #+#             */
-/*   Updated: 2022/08/26 22:02:10 by zhatsago         ###   ########.fr       */
+/*   Updated: 2022/08/27 21:07:47 by zhatsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,78 +17,29 @@ void check_rows()
     int i;
     size_t j;
     
-    i = 0;
-    // printf("%d\n", t_map.tmp_map[4][-12]);
-    while (i < t_map.rows-2)
+    i = 1;
+    while (i < t_map.rows - 1 )
 	{
         j = 1;
-        if (t_map.tmp_map[i][ft_strlen(t_map.tmp_map[i]) - 1] == '0' && t_map.tmp_map[i][ft_strlen(t_map.tmp_map[i]) - 2] != '1' )
-            ft_exit("Error: Open border!");
+        if (t_map.tmp_map[i][ft_strlen(t_map.tmp_map[i]) - 1] == '0' && 
+            t_map.tmp_map[i][ft_strlen(t_map.tmp_map[i]) - 2] != '1')
+            ft_exit("Error: open border!4");
+        if (t_map.tmp_map[i][0] == '0')
+            ft_exit("Error: open border!3");
 		while (j < ft_strlen(t_map.tmp_map[i]) - 2)
 	    {
             if (t_map.tmp_map[i][j] == ' ')
+            {
                 if (t_map.tmp_map[i][j + 1] == '0' || t_map.tmp_map[i][j - 1] == '0')
-                    printf("Error: open border" );
-            
-            // printf("%zu", ft_strlen(t_map.tmp_map[i]));
-          
-            
-            // if (t_map.tmp_map[i][j] == ' ')
-            // {
-            //     if (t_map.tmp_map[i][j+1] == '0' || t_map.tmp_map[i][j - 1] == '0')
-            //         printf("Error: open border" );
-                // if (t_map.tmp_map[i][j+1] == '0' || t_map.tmp_map[i][j - 1] == '0')
-                // {
-                //     printf("error [%d][%zu]- {%d}  [%d][%zu] - {%d}", i, j - 1,
-                //                             t_map.tmp_map[i][j - 1], i, j + 1, t_map.tmp_map[i][j+1]  );
-                // }
-                
-            //}
-            
-            // printf("%d", t_map.tmp_map[i][j]);
+                    ft_exit("Error: open border!1");           
+                if (t_map.tmp_map[i - 1][j] == '0' || t_map.tmp_map[i + 1][j] == '0')
+                    ft_exit("Error: open border!6");           
+            }     
+            if ((t_map.tmp_map[0][j] == '0' || t_map.tmp_map[t_map.rows - 1][j] == '0'))
+                printf("%d", t_map.rows); //not working properly
+                // ft_exit("Error: open border!2");
             j++;
 		}
-        // printf("\n");
-        // while ((int)j < t_map.cols)
-        //     t_map.matrix[i][j] = '-';
-        // t_map.matrix[i][j] = '\0';
 		i++;
     }
-
-
-
-    
-    // while (i < t_map.rows)
-	// {
-    //     j = 0;
-	// 	while (j < ft_strlen(t_map.tmp_map[i]) - 1)
-	//     {
-    //         // if (t_map.tmp_map[i][ft_strlen(t_map.tmp_map[i])] == '0')
-    //         // {
-    //         //     printf("\nError: Open border! %d %zu" ,i, j);
-    //         //     break;
-    //         // }
-    //             // ft_exit("Error: Open border! %d %zu", i, j);
-    //         // printf("%d", t_map.tmp_map[i][j]);
-
-    //         if (t_map.tmp_map[i][j] == 32)
-    //         {
-    //              printf("[%d][%lu] %d\n", i, j + 1, t_map.tmp_map[i][j+1]);
-    //              printf("[%d][%lu] %d\n", i, j + 1, t_map.tmp_map[i][j-1]);
-                
-                
-    //             // if (t_map.tmp_map[i][j + 1] == 48 || t_map.tmp_map[i][j - 1] == 48)
-    //             // {
-    //             //     printf(" prev %c\n", t_map.tmp_map[i][j - 1]);
-    //             //     printf(" aft %c", t_map.tmp_map[i][j + 1]);
-    //             //     printf("\nError: Open border! %d %zu" ,i, j);
-    //             //     break;
-    //             // }
-    //         }
-    //                 // ft_exit("Error: Open border!");
-    //         j++;
-	// 	}
-    //             printf("\n");
-	// 	i++;
-	// }
 }
